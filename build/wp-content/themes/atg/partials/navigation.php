cea717970240965e2ca46b9d5b4f6a08e00fc00a
Jakub Pabis <jakubpabis@Jakubs-MacBook-Pro.local>
2017-10-18 22:45:44 +0100

    some mobile styling for menubar

--- build/wp-content/themes/atg/partials/navigation.php
+++ build/wp-content/themes/atg/partials/navigation.php
@@ -1,50 +1,52 @@
 <nav role="navigation" class="main-navigation">
 	<div class="top">
-		<div class="left">
-			<ul class="social">
-				<li>
-					<a href="">
-						<i class="fa fa-facebook" aria-hidden="true"></i>
-					</a>
-				</li>
-				<li>
-					<a href="">
-						<i class="fa fa-pinterest-p" aria-hidden="true"></i>
-					</a>
-				</li>
-				<li>
-					<a href="">
-						<i class="fa fa-twitter" aria-hidden="true"></i>
-					</a>
-				</li>
-				<li>
-					<a href="">
-						<i class="fa fa-instagram" aria-hidden="true"></i>
-					</a>
-				</li>
-			</ul>
-		</div>
-		<div class="right">
-			<ul class="tools">
-				<li>
-					<a href="">
-						<span>search</span>
-						<i class="material-icons">search</i>
-					</a>
-				</li>
-				<li>
-					<a href="<?= get_home_url(); ?>/my-account">
-						<span>account</span>
-						<i class="material-icons">person</i>
-					</a>
-				</li>
-				<li>
-					<a href="<?= get_home_url(); ?>/cart">
-						<span>basket</span>
-						<i class="material-icons">shopping_basket</i>
-					</a>
-				</li>
-			</ul>
+		<div class="container">
+			<div class="left">
+				<ul class="social">
+					<li>
+						<a href="https://www.facebook.com/allthatglittersest2017/" target="_blank">
+							<i class="fa fa-facebook" aria-hidden="true"></i>
+						</a>
+					</li>
+					<!--<li>
+						<a href="">
+							<i class="fa fa-pinterest-p" aria-hidden="true"></i>
+						</a>
+					</li>
+					<li>
+						<a href="">
+							<i class="fa fa-twitter" aria-hidden="true"></i>
+						</a>
+					</li>-->
+					<li>
+						<a href="https://www.instagram.com/all_that_glitters_est_2017/" target="_blank">
+							<i class="fa fa-instagram" aria-hidden="true"></i>
+						</a>
+					</li>
+				</ul>
+			</div>
+			<div class="right">
+				<ul class="tools">
+					<li>
+						<a href="">
+							<span>search</span>
+							<i class="material-icons">search</i>
+						</a>
+					</li>
+					<li>
+						<a href="<?= get_home_url(); ?>/my-account">
+							<span>account</span>
+							<i class="material-icons">person</i>
+						</a>
+					</li>
+					<li>
+						<a href="<?= get_home_url(); ?>/cart">
+							<span>basket</span>
+							<i class="material-icons">shopping_basket</i>
+						</a>
+					</li>
+				</ul>
+			</div>
 		</div>
 	</div>
 	<div class="bottom">
@@ -53,6 +55,11 @@
 				<a href="/" class="logo">
 					<object data="<?= get_template_directory_uri(); ?>/assets/img/logo.svg" type="image/svg+xml"></object>
 				</a>
+			</div>
+			<div id="menu-btn">
+				<span></span>
+				<span></span>
+				<p>MENU</p>
 			</div>
 			<div class="right">
 				<?php if ( has_nav_menu( 'menu' ) ) :
