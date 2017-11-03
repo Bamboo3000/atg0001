4c455ad46115febf0b3a8199ef11fcf324dd9844
Jakub Pabis <jakubpabis@Jakubs-MacBook-Pro.local>
2017-11-03 17:44:17 +0000

    lookbook add

--- build/wp-content/themes/atg/tpl/tpl-lookbook.php
+++ build/wp-content/themes/atg/tpl/tpl-lookbook.php
@@ -3,122 +3,50 @@
 
 <section class="lookbook">
     <div class="container">
+
+    <?php  for($i = 1; $i <= 10; $i++) : ?>
+
+    <?php if(get_field('lb_image'.$i)) : ?>        
+
         <div class="lookbook__item">
+            <?php if($i % 2 == 0) : ?>
             <div class="image">
-                <img src="assets/img/lb1.jpg" alt="">
+                <?php $image = get_field('lb_image'.$i); ?>
+                <img src="<?php echo $image['url']; ?>" alt="">
             </div>
+            <?php endif; ?>
             <div class="text">
                 <div class="text-container">
+                    <?php $title = get_field('lb_title'.$i); ?>
                     <p class="title">
-                        Summer look
+                        <?php echo $title; ?>
                     </p>
                     <hr>
                     <div class="summary">
+                        <?php $links = get_field('lb_products'.$i); ?>
                         <ul>
-                            <li>
-                                <a href="product-detail.html">Black Hat - <b>£14.99</b></a>
-                            </li>
-                            <li>
-                                <a href="product-detail.html">Coral Necklace - <b>£9.99</b></a>
-                            </li>
-                            <li>
-                                <a href="product-detail.html">Golden Bracelett - <b>£4.99</b></a>
-                            </li>
-                            <li>
-                                <a href="product-detail.html">Pantera styled bag - <b>£24.99</b></a>
-                            </li>
+                            <?php foreach($links as $link) : ?>
+                                <?php $product = new WC_Product( $link->ID ); ?>
+                                <li>
+                                    <a href="product-detail.html"><?php echo $product->name; ?> - <b>£<?php echo number_format($product->price, 2); ?></b></a>
+                                </li>
+                            <?php endforeach; ?>
                         </ul>
                     </div>
                 </div>
             </div>
-        </div>
-        <div class="lookbook__item">
-            <div class="text">
-                <div class="text-container">
-                    <p class="title">
-                        Stylish evening
-                    </p>
-                    <hr>
-                    <div class="summary">
-                        <ul>
-                            <li>
-                                <a href="product-detail.html">Black Hat - <b>£14.99</b></a>
-                            </li>
-                            <li>
-                                <a href="product-detail.html">Coral Necklace - <b>£9.99</b></a>
-                            </li>
-                            <li>
-                                <a href="product-detail.html">Golden Bracelett - <b>£4.99</b></a>
-                            </li>
-                            <li>
-                                <a href="product-detail.html">Pantera styled bag - <b>£24.99</b></a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
+            <?php if($i % 2 != 0) : ?>
             <div class="image">
-                <img src="assets/img/lb2.jpg" alt="">
+                <?php $image = get_field('lb_image'.$i); ?>
+                <img src="<?php echo $image['url']; ?>" alt="">
             </div>
+            <?php endif; ?>
         </div>
-        <div class="lookbook__item">
-            <div class="image">
-                <img src="assets/img/lb3.jpg" alt="">
-            </div>
-            <div class="text">
-                <div class="text-container">
-                    <p class="title">
-                        Sunny morning
-                    </p>
-                    <hr>
-                    <div class="summary">
-                        <ul>
-                            <li>
-                                <a href="product-detail.html">Black Hat - <b>£14.99</b></a>
-                            </li>
-                            <li>
-                                <a href="product-detail.html">Coral Necklace - <b>£9.99</b></a>
-                            </li>
-                            <li>
-                                <a href="product-detail.html">Golden Bracelett - <b>£4.99</b></a>
-                            </li>
-                            <li>
-                                <a href="product-detail.html">Pantera styled bag - <b>£24.99</b></a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="lookbook__item">
-            <div class="text">
-                <div class="text-container">
-                    <p class="title">
-                        Working girl
-                    </p>
-                    <hr>
-                    <div class="summary">
-                        <ul>
-                            <li>
-                                <a href="product-detail.html">Black Hat - <b>£14.99</b></a>
-                            </li>
-                            <li>
-                                <a href="product-detail.html">Coral Necklace - <b>£9.99</b></a>
-                            </li>
-                            <li>
-                                <a href="product-detail.html">Golden Bracelett - <b>£4.99</b></a>
-                            </li>
-                            <li>
-                                <a href="product-detail.html">Pantera styled bag - <b>£24.99</b></a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-            <div class="image">
-                <img src="assets/img/lb6.jpg" alt="">
-            </div>
-        </div>
+
+    <?php endif; ?>  
+
+    <?php endfor; ?>  
+
     </div>
 </section>
 
