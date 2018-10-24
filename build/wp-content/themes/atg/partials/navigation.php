d02df1bb79129106f7ae8d516aee961a7ea2e9f3
Jakub Pabis <pabis91@gmail.com>
2018-10-24 19:16:29 +0200

    small

--- build/wp-content/themes/atg/partials/navigation.php
+++ build/wp-content/themes/atg/partials/navigation.php
@@ -8,16 +8,6 @@
 							<i class="fa fa-facebook" aria-hidden="true"></i>
 						</a>
 					</li>
-					<!--<li>
-						<a href="">
-							<i class="fa fa-pinterest-p" aria-hidden="true"></i>
-						</a>
-					</li>
-					<li>
-						<a href="">
-							<i class="fa fa-twitter" aria-hidden="true"></i>
-						</a>
-					</li>-->
 					<li>
 						<a href="https://www.instagram.com/all_that_glitters_est_2017/" target="_blank">
 							<i class="fa fa-instagram" aria-hidden="true"></i>
